9fb1e8f2b4c96f8e5fc5f24a16da6a454f4fb6bf
Dakshh <pathakdaksh1601@gmail.com>
2025-08-13 01:01:26 +0530

    sol136 Single Number

---
+++ sol136.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { //By adding an & operator we are passing the value by reference
+    int ans = 0;
+    for (int val : nums) {
+        ans = ans ^ val ;
+    }
+    return ans ;
+    }
+};
