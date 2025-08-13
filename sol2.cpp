c787e2b3345aec8bc87ee4959714f36de28d6d1a
Dakshh <pathakdaksh1601@gmail.com>
2025-08-13 16:48:28 +0530

    two sum problem

---
+++ sol2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int sum = carry;
+
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+        }
+        return dummy->next;
+    }
+};
+
+int main() {
+    // Test: (2 -> 4 -> 3) + (5 -> 6 -> 4) = (7 -> 0 -> 8)
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    Solution s;
+    ListNode* result = s.addTwoNumbers(l1, l2);  // <-- correct name + full call
+
+    // print result
+    for (ListNode* p = result; p != nullptr; p = p->next) {
+        cout << p->val << (p->next ? " -> " : "\n");
+    }
+    return 0;
+}
