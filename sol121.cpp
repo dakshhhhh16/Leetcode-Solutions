8ca6af304333338105cb3f10a2bc4204eaa4c290
Dakshh <daksh.pathak.ug24@nsut.ac.in>
2025-08-17 22:12:38 +0530

    sol121

--- sol121.cpp
+++ sol121.cpp
@@ -7,7 +7,7 @@ public:
     int maxProfit(vector<int>& prices) {
         int maxProfit = 0, bestBuy = prices[0] ; //Assuming price of first day to be the best day to buy stocks
 
-        for (int i=1 ;i<prices.size() ; i++) {
+        for (int i=1 ;i<prices.size() ; i++) { //size of the (prices) vector
             if (prices[i] > bestBuy) {
                 maxProfit = max(maxProfit, prices[i] - bestBuy) ;
             }
