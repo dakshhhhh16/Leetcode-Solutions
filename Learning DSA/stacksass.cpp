50807de235d87c054125d766e391b7e8c4d89c29
Dakshh <daksh.pathak.ug24@nsut.ac.in>
2025-09-09 19:31:28 +0530

    assignment

---
+++ Learning DSA/stacksass.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 5  // Maximum size of Stack
+
+class Stack {
+    int top;
+    int arr[MAX];
+
+public:
+    Stack() { top = -1; }
+
+    // Function to check if stack is empty
+    bool isEmpty() {
+        return (top == -1);
+    }
+
+    // Function to check if stack is full
+    bool isFull() {
+        return (top == MAX - 1);
+    }
+
+    // Function to push an element into stack
+    void push(int value) {
+        if (isFull()) {
+            cout << "Stack Overflow! Cannot push " << value << endl;
+        } else {
+            arr[++top] = value;
+            cout << value << " pushed into stack." << endl;
+        }
+    }
+
+    // Function to pop element from stack
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack Underflow! Cannot pop." << endl;
+        } else {
+            cout << arr[top--] << " popped from stack." << endl;
+        }
+    }
+
+    // Function to peek top element
+    void peek() {
+        if (isEmpty()) {
+            cout << "Stack is empty." << endl;
+        } else {
+            cout << "Top element is: " << arr[top] << endl;
+        }
+    }
+};
+
+// Driver Code
+int main() {
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
+
+    s.peek();
+
+    s.pop();
+    s.peek();
+
+    s.push(60);  // Should show overflow if full
+
+    return 0;
+}
