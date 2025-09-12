49f3b440745b6783d04e4467c7ca12906d5d9c21
Dakshh <daksh.pathak.ug24@nsut.ac.in>
2025-09-13 03:08:59 +0530

    making a linked list

---
+++ Learning DSA/linked_list2.cpp
@@ -0,0 +1,10 @@
+//From chatgpt, you can refer the notion file for the actual codes
+//This just tell you about making a linked list in c++
+#include <iostream>
+using namespace std;
+
+//Node Structure
+struct Node {
+    int data;   //value stored in the node
+    Node* next; //pointer to the next node
+};
