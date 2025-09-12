7a4607d1ff499a7db7b14aee7902fd18318fcb14
Dakshh <daksh.pathak.ug24@nsut.ac.in>
2025-09-13 04:42:19 +0530

    Added delete function

--- Learning DSA/funclinkedlist.cpp
+++ Learning DSA/funclinkedlist.cpp
@@ -42,17 +42,46 @@ void insertAtEnd(Node** head_ref, int new_data) {
     temp->next = new_node; // Change the next of last node
 }
 
+// Delete first occurrence of key
+void deleteNode(Node** head_ref, int key) {
+    Node* temp = *head_ref;
+    Node* prev = NULL;
+
+    // If head node itself holds the key
+    if (temp != NULL && temp->data == key) {
+        *head_ref = temp->next; // head updated
+        delete temp;
+        return;
+    }
+
+    // Search for the key
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    // If key not found
+    if (temp == NULL) return;
+
+    // Unlink node
+    prev->next = temp->next;
+    delete temp;
+}
+
+
 int main() {
-    Node* head = NULL;  // empty list
+    Node* head = NULL;
 
-    // Insert elements
-    insertAtBeginning(&head, 10); // 10
-    insertAtBeginning(&head, 20); // 20 -> 10
-    insertAtEnd(&head, 30);       // 20 -> 10 -> 30
-//&head goves the function the ability to udate the head pointer itself
+    insertAtEnd(&head, 10);
+    insertAtEnd(&head, 20);
+    insertAtEnd(&head, 30);
+    printList(head);   // 10 -> 20 -> 30 -> NULL
 
-    // Print list
-    printList(head);
+    insertAtBeginning(&head, 5);
+    printList(head);   // 5 -> 10 -> 20 -> 30 -> NULL
+
+    deleteNode(&head, 20);
+    printList(head);   // 5 -> 10 -> 30 -> NULL
 
     return 0;
 }
